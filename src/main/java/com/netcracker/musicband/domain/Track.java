c6635f3b008e2d2b24543eee9554f5400bfeb7c4
Alexander Sukhikh <xbox24_772@mail.ru>
2019-10-30 17:34:11 +0300

    Decided to save track in Map, not in JSON

--- src/main/java/com/netcracker/musicband/domain/Track.java
+++ src/main/java/com/netcracker/musicband/domain/Track.java
@@ -1,12 +1,14 @@
 package com.netcracker.musicband.domain;
 
+import org.springframework.data.util.Pair;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
+import java.util.Map;
 import java.util.Vector;
-import javafx.util.Pair;
 
 @Entity
 public class Track {
@@ -16,8 +18,37 @@ public class Track {
     private int id;
     private String name;
     private Date createDate;
-
-    //TODO переделать, пока хз во что упаковать json
     private Map<String, Vector<Pair<Character,Integer>>> track;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Map<String, Vector<Pair<Character, Integer>>> getTrack() {
+        return track;
+    }
+
+    public void setTrack(Map<String, Vector<Pair<Character, Integer>>> track) {
+        this.track = track;
+    }
 }
